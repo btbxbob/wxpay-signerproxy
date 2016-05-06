f54e8143f2507896eaba22c44c2ef7e89c629551
BX Xiang <btbxbob@gmail.com>
2016-05-06 15:27:00 +0800

    added cert mode

--- wxpay-signerproxy.go
+++ wxpay-signerproxy.go
@@ -3,6 +3,8 @@ package main
 
 import (
 	"bytes"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"io/ioutil"
 	"log"
@@ -21,24 +23,34 @@ import (
 // Configuration to load from json
 type Configuration struct {
 	// listen address, like:"0.0.0.0:80"
-	Listen string
+	Listen struct {
+		HTTP  string
+		HTTPS string
+	}
 	// Key 秘钥，在商户平台的API安全里设置
 	Key string
+	// Resign 是否重签名
+	UseCert bool `json:"use_cert"`
+	Cert    struct {
+		CertFile string
+		KeyFile  string
+		Ca       string
+	}
+	Resign bool
+	// LogToFile 是否记录到文件
+	LogToFile bool `json:"log_to_file"`
+	// Diagnosis 是否诊断
+	Diagnosis bool
 	// IsLoad 判断是否已经载入配置
 	IsLoad bool
 }
 
-var config = Configuration{}
+var config Configuration
+var cert tls.Certificate
+var caCert []byte
 
 func init() {
 	// init logger
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	log.SetOutput(&lumberjack.Logger{
-		Filename:   "log.txt",
-		MaxSize:    20,
-		MaxBackups: 9,
-	})
-
 	config.IsLoad = false
 	configFile, _ := os.Open("config.json")
 	defer configFile.Close()
@@ -48,53 +60,87 @@ func init() {
 	if err != nil {
 		log.Println("error:", err)
 	}
-	log.Println("listen address:", config.Listen)
+	log.Println("listen address:", config.Listen.HTTP, config.Listen.HTTPS)
 	config.IsLoad = true
+
+	if config.LogToFile {
+		log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+		l := &lumberjack.Logger{
+			Filename:  "log.txt",
+			MaxSize:   20,
+			LocalTime: true,
+		}
+		l.Rotate()
+		log.SetOutput(l)
+	}
+
+	//载入证书
+	if config.UseCert {
+		// Load client cert
+		cert, err = tls.LoadX509KeyPair(config.Cert.CertFile, config.Cert.KeyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Load CA cert
+		caCert, err = ioutil.ReadFile(config.Cert.Ca)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 func main() {
-	// do some test here
-	// 0. Collect Infos
-	log.Println("[INFO] ============")
-	log.Println("[INFO] 0. Address Info")
-	localAddrs, err := net.InterfaceAddrs()
-	log.Printf("%v", localAddrs)
-	// 1. DNS lookup test
-	log.Println("[INFO] ============")
-	log.Println("[INFO] 1. DNS Lookup test")
-	addr, err := net.LookupHost("api.mch.weixin.qq.com")
-	log.Printf("[INFO] Local DNS lookup result: %v", addr)
+	if config.Diagnosis {
+		// do some test here
+		// 0. Collect Infos
+		log.Println("[INFO] ============")
+		log.Println("[INFO] 0. Address Info")
+		localAddrs, err := net.InterfaceAddrs()
+		log.Printf("%v", localAddrs)
+		// 1. DNS lookup test
+		log.Println("[INFO] ============")
+		log.Println("[INFO] 1. DNS Lookup test")
+		addr, err := net.LookupHost("api.mch.weixin.qq.com")
+		log.Printf("[INFO] Local DNS lookup result: %v", addr)
 
-	c := dns.Client{}
-	m := dns.Msg{}
-	m.SetQuestion("api.mch.weixin.qq.com.", dns.TypeA)
-	r, t, err := c.Exchange(&m, "119.29.29.29:53")
-	if err != nil {
-		log.Printf("[FATAL] Get ip from 119.29.29.29 failed. err:%s", err)
-	}
-	log.Printf("[INFO] Request took time: %v", t)
-	if len(r.Answer) == 0 {
-		log.Fatal("No results")
-	}
-	log.Printf("[INFO] Remote DNS lookup result:%v", r)
-	//2. local time and remote time
-	log.Println("[INFO] ============")
-	log.Println("[INFO] 2. Time test")
-	localTime := time.Now()
-	log.Printf("[INFO] Local Time:%v", localTime.Unix())
-	log.Println("[INFO] Getting remote time, should not wait too long")
-	remoteTime, err := ntp.Time("cn.pool.ntp.org")
-	if err != nil {
-		log.Printf("[FATAL] Get time from cn.pool.ntp.org failed. err:%s", err)
-	}
-	log.Printf("[INFO] Remote Time:%v", remoteTime.Unix())
-	if math.Abs(float64(localTime.Unix()-remoteTime.Unix())) > 10 {
-		log.Printf("[WARNING] Time needs Sync:%v", remoteTime.Unix())
-	}
+		c := dns.Client{}
+		m := dns.Msg{}
+		m.SetQuestion("api.mch.weixin.qq.com.", dns.TypeA)
+		r, t, err := c.Exchange(&m, "119.29.29.29:53")
+		if err != nil {
+			log.Printf("[FATAL] Get ip from 119.29.29.29 failed. err:%s", err)
+		}
+		log.Printf("[INFO] Request took time: %v", t)
+		if len(r.Answer) == 0 {
+			log.Fatal("No results")
+		}
+		log.Printf("[INFO] Remote DNS lookup result:%v", r)
+		//2. local time and remote time
+		log.Println("[INFO] ============")
+		log.Println("[INFO] 2. Time test")
+		localTime := time.Now()
+		log.Printf("[INFO] Local Time:%v", localTime.Unix())
+		log.Println("[INFO] Getting remote time, should not wait too long")
+		remoteTime, err := ntp.Time("cn.pool.ntp.org")
+		if err != nil {
+			log.Printf("[FATAL] Get time from cn.pool.ntp.org failed. err:%s", err)
+		}
+		log.Printf("[INFO] Remote Time:%v", remoteTime.Unix())
+		if math.Abs(float64(localTime.Unix()-remoteTime.Unix())) > 10 {
+			log.Printf("[WARNING] Time needs Sync:%v", remoteTime.Unix())
+		}
 
-	log.Println("[INFO] Test done.")
+		log.Println("[INFO] Test done.")
+	}
+	go func() {
+		err2 := http.ListenAndServe(config.Listen.HTTP, http.HandlerFunc(mainHandler))
+		if err2 != nil {
+			log.Fatal("ListenAndServe:", err2)
+		}
+	}()
 
-	err = http.ListenAndServeTLS(config.Listen, "server.crt", "server.key", http.HandlerFunc(mainHandler))
+	err := http.ListenAndServeTLS(config.Listen.HTTPS, "server.crt", "server.key", http.HandlerFunc(mainHandler))
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
@@ -107,7 +153,7 @@ func mainHandler(w http.ResponseWriter, req *http.Request) {
 	body, err := ioutil.ReadAll(req.Body)
 	defer req.Body.Close()
 	mapData, err := XMLStructToMap(body)
-	log.Printf("[INFO] Request content:%v", string(body))
+	log.Printf("[INFO][IN] Request content:%v", string(body))
 
 	// sign
 	eq, err := CompareSignature(body)
@@ -116,10 +162,13 @@ func mainHandler(w http.ResponseWriter, req *http.Request) {
 		log.Printf("original sign: %s", mapData["sign"])
 		newSign, err2 := CalculateSignature(mapData, config.Key)
 		log.Printf("calculated sign: %s", newSign)
-		mapData["sign"] = newSign
-		body, err2 = MapToXML(mapData)
-		if err2 != nil {
-			log.Fatal(err.Error())
+		if config.Resign {
+			mapData["sign"] = newSign
+			body, err2 = MapToXML(mapData)
+			if err2 != nil {
+				log.Fatal(err.Error())
+			}
+			log.Printf("[INFO][IN] Renewed request content:%v", string(body))
 		}
 	}
 
@@ -127,10 +176,25 @@ func mainHandler(w http.ResponseWriter, req *http.Request) {
 	newReq.Header = req.Header
 	newReq.URL.Host = "api.mch.weixin.qq.com"
 	newReq.Host = newReq.URL.Host
-	//newReq.Body = body
 	newReq.URL.Scheme = "https"
 
-	client := &http.Client{}
+	var client *http.Client
+	//cert
+	if config.UseCert {
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+		// Setup HTTPS client
+		tlsConfig := &tls.Config{
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      caCertPool,
+		}
+		tlsConfig.BuildNameToCertificate()
+		transport := &http.Transport{TLSClientConfig: tlsConfig}
+		client = &http.Client{Transport: transport}
+	} else {
+		client = &http.Client{}
+	}
+
 	resp, err := client.Do(newReq)
 
 	if err != nil {
@@ -141,7 +205,7 @@ func mainHandler(w http.ResponseWriter, req *http.Request) {
 
 	//io.Copy(w, resp.Body)
 	respBody, err := ioutil.ReadAll(resp.Body)
-	log.Printf("[INFO] Respond content:%v", string(respBody))
+	log.Printf("[INFO][IN] Respond content:%v", string(respBody))
 	w.Write(respBody)
 	defer resp.Body.Close()
 	if err != nil {
